f4554fd90817fcd8c56ea041537122fd72c7889d
unknown <jakab.attila2001@gmail.com>
2022-02-10 12:09:16 +0100

    kész

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,7 +14,8 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
     this.loginForm = new FormGroup({
       email: new FormControl('',[Validators.required,Validators.email]),
-      password: new FormControl('',Validators.required)
+      password: new FormControl('',Validators.required),
+      name: new FormControl('', [Validators.required,Validators.pattern('^[a-zA-Z0-9+ ]$')])
     })
   }
 
